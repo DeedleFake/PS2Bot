e5b1ae1f68356ea6d93180934121e35eb6c5a5fc
DeedleFake <deedlefake@users.noreply.github.com>
2015-06-17 13:08:09 -0400

    Begin working on Census support.

--- census.go
+++ census.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
 )
 
 const (
 	URLCensusChar     = "http://census.daybreakgames.com/s:vAPP/get/ps2:v2/character/?name.first=%s&c:case=false&c:resolve=stat_history,faction,world,outfit_member_extended"
 	URLCensusCharStat = "http://census.daybreakgames.com/s:vAPP/get/ps2:v2/characters_stat?character_id=%s&c:limit=5000"
+)
+
+var (
+	NoSuchCharacterErr = errors.New("No such character")
 )
 
 type Server string
@@ -28,4 +35,35 @@ func (s Server) String() string {
 	}
 
 	panic("Unknown server: " + s)
+}
+
+type CensusChar struct {
+}
+
+func GenReport(char string) (*CensusChar, error) {
+	charURL := fmt.Sprintf(URLCensusChar, char)
+
+	rsp, err := http.Get(charURL)
+	if err != nil {
+		return nil, err
+	}
+	defer rsp.Body.Close()
+
+	d := json.NewDecoder(rsp.Body)
+
+	var cc struct {
+		CharacterList []struct {
+		} `json:"character_list"`
+		Returned int `json:"returned"`
+	}
+
+	err = d.Decode(&cc)
+	if err != nil {
+		return nil, err
+	}
+	if cc.Returned == 0 {
+		return nil, NoSuchCharacterErr
+	}
+
+	return nil, NoSuchCharacterErr
 }
